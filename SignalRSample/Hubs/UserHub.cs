433177c153d25165b8222c6b68ee638b246a142d
Johan Selin <johanselin99@gmail.com>
2023-02-11 21:50:49 +0100

    initial commit (view count functionality)

---
+++ SignalRSample/Hubs/UserHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs;
+
+public class UserHub : Hub
+{
+    public static int TotalViews { get; set; } = 0;
+
+    public async Task NewWindowLoaded()
+    {
+        TotalViews++;
+        // Send update to all clients that total views have been updated.
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+    }
+}
