f867bcd51d8488e17aa0b8429513d94b98a50057
Johan Selin <johanselin99@gmail.com>
2023-02-14 11:09:02 +0100

    Support for counting simultaneous connections

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -6,6 +6,24 @@ public class UserHub : Hub
 {
     public static int TotalViews { get; set; } = 0;
 
+    public static int ConnectedUsers { get; set; } = 0;
+
+    public override Task OnConnectedAsync()
+    {
+        ConnectedUsers++;
+        // GetAwaiter instead of await keyword since method is not async
+        Clients.All.SendAsync("updateConnectedUsers", ConnectedUsers).GetAwaiter().GetResult();
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        ConnectedUsers--;
+        // GetAwaiter instead of await keyword since method is not async
+        Clients.All.SendAsync("updateConnectedUsers", ConnectedUsers).GetAwaiter().GetResult();
+        return base.OnDisconnectedAsync(exception); 
+    }
+
     public async Task NewWindowLoaded()
     {
         TotalViews++;
